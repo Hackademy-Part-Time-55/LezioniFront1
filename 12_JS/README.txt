82e8e2aae91c204b91a7e898fa257df5be4208c1
kutta-dev <giuseppe.calia90@gmail.com>
2025-02-07 21:34:16 +0100

    js variabili

---
+++ 12_JS/README.txt
@@ -0,0 +1,48 @@
+JavaScript:
+- è un linguaggio interpretato (a differenza dei linguaggi compilati) ed eseguito da un componente del browser che si chiama Engine JS
+- è un linguaggio di alto livello
+- è un linguaggio a tipizzazione dinamica
+
+Come possiamo include JS nel file html?
+3 modi:
+- inline
+- embedded
+- file esterno
+
+
+Sintassi:
+- case sensitive
+
+
+Tipi di dato:
+- primitivi
+- riferimento
+
+Tipi di dato primitivi:
+- number
+- boolean
+- string
+
+
+Operatori:
+
+operatori aritmetici
++
+-
+*
+/
+%
+
+
+operatori di incremento e decremento
+++
+--
+
+
+=  assegnamento (con precedenza più bassa rispetto agli altri operatori)
+
+operatori di assegnamento composti:
++=
+-=
+*=
+/=
---
+++ 12_JS/index.js
@@ -0,0 +1,212 @@
+/*
+// dichiarazione di una variabile
+var x;
+
+// operazione di assegnamento (assegnare un valore ad una variabile significa scrivere un valore nell'area di memoria)
+// = operatore di assegnamento
+x = 3;
+
+console.log(x);
+*/
+
+/*
+var x;
+
+x = 3;
+
+x = 99; // sovrascrittura del vecchio valore con il nuovo valore
+
+console.log(x);
+*/
+
+
+/*
+var x;
+x = 3;
+
+var y;
+y = x;
+
+x = 99;
+
+console.log(x); // 99
+console.log(y); // 3 
+*/
+
+/*
+var x;
+console.log(x); // undefined
+*/
+
+/*
+// dichiarazione e inizializzazione di una variabile
+var x = 0;
+
+x = 5;
+
+console.log(x);
+*/
+
+/*
+var x = 0;
+console.log(x);
+
+x = 5;
+console.log(x);
+*/
+
+
+
+/*
+var x;
+x = true;
+// x = 'ciao';
+// x = "ciao";
+
+console.log(x);
+*/
+
+
+/*
+var x;
+
+x = 3;  // number
+console.log(x);
+
+x = 'ciao'; // string
+console.log(x);
+*/
+
+/*
+// snake case
+var nome_completo_utente;
+nome_completo_utente = "ugo";
+
+console.log(nome_completo_utente);
+
+// camel case
+var nomeCompletoUtente;
+nomeCompletoUtente = "ugo";
+
+console.log(nomeCompletoUtente);
+*/
+
+
+/*
+var firstname;
+firstname = 'Eric';
+
+console.log(firstname);
+*/
+
+
+
+
+
+/*
+let firstname;
+firstname = 'Eric';
+console.log(firstname);
+
+
+firstname = 'Erika';
+console.log(firstname);
+*/
+
+
+
+/*
+const firstname = 'Eric';
+console.log(firstname);
+console.log(firstname);
+console.log(firstname);
+
+firstname = 'Erika';    // erroreeeee
+console.log(firstname);
+*/
+
+
+
+/*
+var firstname = 'Eric';
+console.log(typeof firstname);
+*/
+
+/*
+var x;
+x = 5;
+
+var y;
+y = 3;
+
+var w;
+w = 2;
+
+var z;
+z = (x + y) * w;
+
+console.log(z);
+*/
+
+
+/*
+var x;
+x = 7;
+
+var y;
+y = 2;
+
+var z;
+z = x % y;
+
+console.log(z);
+*/
+
+
+/*
+var x;
+x = 3;
+
+// x = x + 2;
+// x += 2;
+
+// x = x - 2;
+// x -= 2;
+
+console.log(x);
+*/
+
+
+/*
+var x;
+x = 3;
+
+// x = x + 1;
+// x++;
+
+// x = x - 1;
+x--;
+
+console.log(x);
+*/
+
+/*
+var x;
+x = 3;
+
+var y;
+
+++x;
+y = x;
+
+// x = x + 1;
+// y = x;
+// y = ++x;
+
+// y = x;
+// x = x + 1;
+// y = x++;
+
+console.log(x);
+console.log(y);
+*/
